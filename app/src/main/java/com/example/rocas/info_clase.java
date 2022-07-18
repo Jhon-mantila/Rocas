1182d7564de355b1518cef2daefe148dde9ffddd
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-07-17 21:37:43 -0500

    Actividades nuevas(cambiar entre actividades)
    1. Crear nuevo layout
    2. Crear una nueva clase java.
    3. Activar nueva actividad (especificar donde se activara la actividad)
    4. Registrar la activdada en la app ( Se  registra en AndroidManifiest)

---
+++ app/src/main/java/com/example/rocas/info_clase.java
@@ -0,0 +1,12 @@
+package com.example.rocas;
+
+import android.app.Activity;
+import android.os.Bundle;
+//Creando la clase para la actividad info
+public class info_clase extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+}
