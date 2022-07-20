4fa0019e8add0dce7e139883c2c6b05a518eba43
Jhon Mantilla <jhon.e.mantilla@gmail.com>
2022-07-20 11:10:11 -0500

    *Crear el menú 3 puntos 1. crear nuevo archivo sml resuces tipo menu 2. configurar las opciones de menu:id y titulo
    *Activar el menú
    1. Agregar el menú a mainActivity.java
    2. Configurar la acción según la opción selecionada

--- app/src/main/java/com/example/rocas/MainActivity.java
+++ app/src/main/java/com/example/rocas/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,4 +28,39 @@ public class MainActivity extends AppCompatActivity {
     public void salir_app(View vista){
         finish();
     }
+
+    //Activar el Menu de  tres puntos
+    //sobre escribir metodos de activity
+    @Override public boolean onCreateOptionsMenu(Menu mimenu){
+
+        getMenuInflater().inflate(R.menu.menu_in_activity, mimenu);
+
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem opcion_menu){
+
+        int id = opcion_menu.getItemId();
+
+        if (id == R.id.configuracion){
+
+            return true;
+
+        }else if(id == R.id.info){
+            //no le paso vista
+            ejecutar_info(null);
+            return true;
+
+        }else if(id == R.id.salir){
+
+            salir_app(null);
+            return true;
+
+        }else{
+
+            return super.onOptionsItemSelected(opcion_menu);
+        }
+    }
+
+    //Aca termina el menu de tres puntos....
 }
